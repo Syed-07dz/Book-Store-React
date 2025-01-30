c28a71b1384c6ffca0d920504b38d4e86981f56a
Syed-07dz <syedaman9504@gmail.com>
2025-01-30 23:00:28 +0530

    react project

---
+++ BookStore/Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./components/Navbar.jsx";
+import Banner from "./components/Banner.jsx"  
+function App() {
+  return (<>
+  <Navbar />
+  
+<Banner/>
+
+  </>
+   );
+}
+
+export default App;
