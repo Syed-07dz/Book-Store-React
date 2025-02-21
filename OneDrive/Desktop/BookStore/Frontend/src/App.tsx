edc44aa6aefc74e5aa7b015544c0d901a8436df9
syed-07dz <syedaman9504@gmail.com>
2025-02-21 18:37:46 +0530

    updates

--- OneDrive/Desktop/BookStore/Frontend/src/App.tsx
+++ OneDrive/Desktop/BookStore/Frontend/src/App.tsx
@@ -4,13 +4,15 @@ import { Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses"
 
 function App() {
-  return (<>
- {/*<Home/>
- <Course/>*/}
+  return (
+  <>
+ <div className="dark:bg-slate-900 dark:text-white">
  <Routes>
   <Route path="/" element={<Home/>}/>
   <Route path="/course" element={<Courses />} /> 
  </Routes>
+ </div>
+ 
   </>
    );
 }
