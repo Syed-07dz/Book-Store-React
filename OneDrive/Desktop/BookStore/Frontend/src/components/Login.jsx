5ec1038daa257a60d5742101f5357293c607315e
syed-07dz <syedaman9504@gmail.com>
2025-02-25 22:33:35 +0530

    signup

--- OneDrive/Desktop/BookStore/Frontend/src/components/Login.jsx
+++ OneDrive/Desktop/BookStore/Frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
   return (
@@ -7,31 +7,46 @@ function Login() {
   <div className="modal-box">
     <form method="dialog">
       {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <Link to="/" 
+      className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
     </form>
     <h3 className="font-bold text-lg">Login!</h3>
     {/* Email */}
-    <div className="mt-4 space-y-1">
+    <div className="mt-4 space-y-2">
         <span>Email</span>
         <br/>
-        <input type="email" placeholder="Enter your email" className="w-80 px-3 py-1 border-md outline-none"/>
+        <input type="email" 
+        placeholder="Enter your email" 
+        className="w-80 px-3 py-1 border-md outline-none"
+        />
     </div>
     {/* Password */}
-    <div className="mt-4 space-y-1">
+    <div className="mt-4 space-y-2">
         <span>Password</span>
         <br/>
-        <input type="text" placeholder="Enter your password" className="w-80 px-3 py-1 border-md outline-none"/>
+        <input type="text" 
+        placeholder="Enter your password"
+         className="w-80 px-3 py-1 border-md outline-none"
+         />
     </div>
-   {/* Button */}
-   <div className="flex justify-around mt-4">
-    <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
-        Login</button>
-    <p>Not register?<span className="underline text-blue-500 cursor-pointer">Signup</span> </p>
-   </div>
-  </div>
-</dialog>
+    {/* Button */}
+     <div className="flex justify-around mt-4">
+     <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+        Login
+      </button>
+     <p>
+      Not register?
+       <Link 
+       to ="/signup"
+        className="underline text-blue-500 cursor-pointer">
+      Signup
+      </Link>{" "} 
+    </p>
+      </div>
     </div>
-  );
+   </dialog>
+ </div>
+);
 }
 
 export default Login
